24b83a9d648e605c04b2ebfa7b3eab5b09a1f45d
John Crickett <john.crickett@aria-networks.com>
2016-03-16 11:56:04 +0000

    Refactoring to functions

--- train_model.py
+++ train_model.py
@@ -4,58 +4,79 @@ from pybrain.structure import FullConnection
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
-import numpy
 import pandas
 
 
-#data = numpy.loadtxt('./data/train.csv', delimiter=',',skiprows=1)
-#num_features = len(data[0]) - 2
-data = pandas.read_csv('./data/train.csv', delimiter=',')
-num_features = len(data.columns) - 3
-
-network = FeedForwardNetwork()
-inLayer = LinearLayer(num_features)
-#hiddenLayer = SigmoidLayer(num_features * 2)
-hiddenLayer = ReluLayer(num_features)
-outLayer = LinearLayer(1)
-
-network.addInputModule(inLayer)
-network.addModule(hiddenLayer)
-network.addOutputModule(outLayer)
-
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
-network.addConnection(in_to_hidden)
-network.addConnection(hidden_to_out)
-network.sortModules()
+def normalise(data_high, data_low, range_high, range_low, value):
+    return (((val - data_low) * (range_high - range_low)) / (data_high - data_low)) + range_low
 
 
-# normalise the data?
+def build_network(num_features):
+    network = FeedForwardNetwork()
+    inLayer = LinearLayer(num_features)
+    #hiddenLayer = SigmoidLayer(num_features * 2)
+    hiddenLayer = ReluLayer(num_features)
+    outLayer = LinearLayer(1)
+
+    network.addInputModule(inLayer)
+    network.addModule(hiddenLayer)
+    network.addOutputModule(outLayer)
+
+    in_to_hidden = FullConnection(inLayer, hiddenLayer)
+    hidden_to_out = FullConnection(hiddenLayer, outLayer)
+    network.addConnection(in_to_hidden)
+    network.addConnection(hidden_to_out)
+    network.sortModules()
+    return network
 
 
 
-# build the training data set
-ds = SupervisedDataSet(num_features, 1)
-#for row in data:
-#    ds.addSample((row[1:-1]), tuple(row[-1:]))
-for tuple_row in data.itertuples():
-    #ds.addSample((row[1:-1]), tuple(row[-1:]))
-    row = list(tuple_row)
-#    print(row[3:])
-    ds.addSample((row[3:-1]), tuple(row[-1:]))
+
+if __name__ == '__main__':
+    data = pandas.read_csv('./data/train.csv', delimiter=',')
+    num_features = len(data.columns) - 3
+
+    network = build_network(num_features)
+
+    # normalise the data?
+    # calculate range for each column
+    max_age = data['Age'].max()
+    min_age = data['Age'].min()
+    print("Max age: " + str(max_age))
+    print("Min age: " + str(min_age))
+    max_class = data['Pclass'].max()
+    min_class = data['Pclass'].min()
+    print("Max class: " + str(max_class))
+    print("Min class: " + str(min_class))
+
+    min_age_nrange = 0
+    max_age_nrange = max_age
+    max_class_nrange = max_class
+    min_class_nrange = min_class
+
+    # build the training data set
+    ds = SupervisedDataSet(num_features, 1)
+    for tuple_row in data.itertuples():
+        row = list(tuple_row)
+        ds.addSample((row[3:-1]), tuple(row[-1:]))
 
 
-trainer = BackpropTrainer(network, ds)
+    trainer = BackpropTrainer(network, ds)
 
-for i in range(5):
-    error = trainer.train()
-    print('Training iteration: {0} error {1}\r                   '.format(i, error), end='')
+    for i in range(5):
+        error = trainer.train()
+        print('\rTraining iteration: {0} error {1}\t\t\t\t\t'.format(i, error), end='')
 
-print()
-# calc error on TS
-test = data[1:51]
+    print()
 
-for tuple_row in data.itertuples():
-    row = list(tuple_row)
-    result = network.activate((row[3:-1]))
-    print('Result: ' + str(result) + ' expected:' + str(row[-1:]))
+    correct = 0
+
+    for tuple_row in data.itertuples():
+        row = list(tuple_row)
+        result = network.activate((row[3:-1]))
+        if result == row[-1]:
+            correct += 1
+
+    accuracy = correct / len(data)
+    print("Score: " + str(accuracy))
+
